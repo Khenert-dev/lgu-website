3b3958d489264fd0a264f71240ff2ba84369ecc4
khenert <2025-1-0957.catayao@kcp.edu.ph>
2026-01-29 15:03:17 +0800

    UI FIXES

--- src/components/sections/BarangayMap.tsx
+++ src/components/sections/BarangayMap.tsx
@@ -1,22 +1,11 @@
 "use client"
 
 import dynamic from "next/dynamic"
-import L from "leaflet"
-import "leaflet/dist/leaflet.css"
 
-delete (L.Icon.Default.prototype as any)._getIconUrl
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-})
-
-const LeafletMap = dynamic(() => import("./LeafletMap"), {
-  ssr: false,
-})
+const LeafletMap = dynamic(
+  () => import("./LeafletMap"),
+  { ssr: false }
+)
 
 export default function BarangayMap() {
   return (
--- src/components/sections/LeafletMap.tsx
+++ src/components/sections/LeafletMap.tsx
@@ -12,24 +12,16 @@ export default function LeafletMap() {
 
     const map = L.map(mapRef.current).setView([16.45, 120.59], 13)
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap contributors",
-    }).addTo(map)
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
 
     L.marker([16.45, 120.59])
       .addTo(map)
       .bindPopup("La Trinidad")
-      .openPopup()
 
     return () => {
       map.remove()
     }
   }, [])
 
-  return (
-    <div
-      ref={mapRef}
-      style={{ height: "100%", width: "100%" }}
-    />
-  )
+  return <div ref={mapRef} className="h-full w-full" />
 }
